04bbfdc362a1641080c62f3af6322848b79ee78e
Rico Castelo <rcastelo@rowetechinc.com>
2020-04-29 13:36:42 -0700

    Working on serial port comms

---
+++ README.md
@@ -0,0 +1,16 @@
+DataloggR
+
+Software to work with the RTI Datalogger
+
+# Run Application
+Run the application by running app.py
+ * This will start a Flask Server and open the web browser
+ * The application will then interact between app.py and the web browser
+
+# Dependencies
+Dependencies are found in requirements.txt
+```bash
+pip install -r requirements.txt
+pip install -r rti_python\requirements.txt
+```
+
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask import render_template
 from flaskwebgui import FlaskUI
+from rti_python.Datalogger.DataloggerHardware import DataLoggerHardware
 
 # RUN THE APP
 # Windows
@@ -14,30 +15,50 @@ from flaskwebgui import FlaskUI
 app = Flask(__name__)
 ui = FlaskUI(app)
 
+# Datalogger hardward
+logger_hardware = DataLoggerHardware()
+
 
 @app.route("/")
 def main_page():
-
-    comm_port_list = ["COM3", "COMM4"]
-    return render_template("download.j2", comm_ports=comm_port_list)
+    # Use Download page as main
+    return download_page(None, None)
 
 
 @app.route("/download")
-def download_page(selected_comm: str):
-    comm_port_list = ["COM3", "COMM4"]
-    print("Selected Port: " + selected_comm)
+def download_page(selected_comm: str, selected_baud: str):
+    comm_port_list = logger_hardware.get_serial_ports()
+    baud_list = logger_hardware.get_baud_rates()
+    if selected_comm:
+        print("Selected Port: " + selected_comm)
+    if selected_baud:
+        print("Selected Port: " + selected_baud)
+
     # Remove the duplicate selected comm port
     if selected_comm in comm_port_list:
         comm_port_list.remove(selected_comm)
+    if selected_baud in baud_list:
+        baud_list.remove(selected_baud)
 
-    return render_template("download.j2", comm_ports=comm_port_list, selected_comm_port=selected_comm)
+    return render_template("download.j2", comm_ports=comm_port_list, bauds=baud_list, selected_comm_port=selected_comm, selected_baud=selected_baud)
 
 
 @app.route("/serial_connect", methods=['POST'])
 def serial_connect():
-    selected_comm_port = request.form.get('comm_port_select')
+    selected_comm_port = request.form.get('comm_port_selected')
+    selected_baud = request.form.get('baud_selected')
     print(selected_comm_port)
-    return download_page(selected_comm_port)
+    print(selected_baud)
+    return download_page(selected_comm_port, selected_baud)
+
+
+@app.route("/scan_serial", methods=['POST'])
+def scan_serial():
+    selected_comm_port = request.form.get('comm_port_selected')
+    selected_baud = request.form.get('baud_selected')
+    print(selected_comm_port)
+    print(selected_baud)
+    return download_page(selected_comm_port, selected_baud)
 
 
 # Run the flask APP
